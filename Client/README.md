541ebd81336ba55686094081e386c54d6ca755cf
Ritu Sharma <100413862+Ritu84@users.noreply.github.com>
2024-06-01 17:02:56 +0530

    Add files via upload

---
+++ Client/README.md
@@ -0,0 +1,61 @@
+# Contact Form
+
+This is a simple React JS and CSS contact form web application. It provides a user-friendly interface for users to submit their contact information and messages.
+
+## Purpose
+
+The purpose of this project is to create a contact form web application that can be easily integrated into any website or web application. It allows users to get in touch with the website owner or administrator by submitting their contact details and messages.
+
+
+## Features
+
+- Responsive and visually appealing design
+- Input validation to ensure the correctness of user-provided information
+- Easy integration into existing websites or web applications
+- User-friendly interface for submitting contact information and messages
+
+## Getting Started
+
+To get started with this project, follow the steps below:
+
+### Prerequisites
+Make sure you have the following software installed on your local system:
+
+- Node.js
+- npm (Node Package Manager)
+
+### Run Locally
+To set up the project on your local system, follow these steps:
+
+1. Clone the repository to your local machine.
+2. Navigate to the project directory in your terminal or command prompt.
+3. Install the dependencies by running the following command:
+
+```bash
+  npm install
+```
+
+4. Once the installation is complete, start the development server:
+
+```bash
+  npm start
+```
+
+5. Open your web browser and visit 'http://localhost:3000' to access the contact form web application.
+
+
+## Usage
+
+To use the contact form web application, follow these steps:
+
+1. Fill in the required contact information, including name, email, and message.
+2. Make sure to provide accurate and valid information.
+3. Click the "Submit" button to send the contact form.
+4. If the form is submitted successfully, you will receive a confirmation message.
+
+![Screenshot (34)](https://github.com/Pranav-Programmer/Contact-Form/assets/79044490/a3f50785-b30f-4bd9-802e-0a402f1a7576)
+
+
+## Contributing
+
+Contributions to this project are welcome. If you find any issues or have suggestions for improvements, please open an issue or submit a pull request. Your feedback and contributions will help make this project better.
---
+++ Client/src/contact.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import './contact.css';
+import { useState } from 'react';
+import Typed from 'react-typed';
+import axios from "axios";
+import Location from './img/location.png';
+import Shape from './img/shape.png';
+import GreenCard from './img/greencard.jpg';
+
+function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const sendEmail = async(e) => {
+    e.preventDefault();
+
+    const dataToSend = {
+      name,
+      email,
+      message
+    };
+    try {
+      const response = await axios.post('https://form-server-1-duby.onrender.com', dataToSend);
+      alert(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <div className='info-section'>
+        <div className="info-text">
+          {/* <h2> Green Cards</h2> */}
+          <div >  
+        <Typed strings={['GREEN CARDS']} typeSpeed={250}/>
+      </div>
+          <p>
+            Green cards, officially known as Permanent Resident Cards, allow non-U.S. citizens to live and work permanently in the United States. Holding a green card is a prerequisite for applying for U.S. citizenship through the naturalization process, typically after five years of residency.
+          </p>
+          <p>
+            Having a green card grants you the legal right to work and live in the United States indefinitely, as long as you do not commit any actions that would make you removable under the immigration law. Green card holders also have the privilege of sponsoring certain relatives for green cards.
+          </p>
+          <p>
+            It is essential to maintain your permanent resident status, including adhering to the tax laws and maintaining continuous residence in the U.S. Applying for a green card typically involves a complex process that includes petitioning by a family member or employer, and it may involve waiting for a visa number to become available.
+          </p>
+        </div>
+        <div className="info-image">
+          <img src={GreenCard} alt="Green Card Information" />
+        </div>
+      </div>
+      <div className='separator'></div>
+      <div className='All-header'>  
+        <Typed strings={['CONTACT']} typeSpeed={250}/>
+      </div>
+      <div className="container-c">
+        <span className="big-circle"></span>
+        <img src={Shape} className="square" alt="" />
+        <div className="form" data-aos="flip-left" data-aos-duration="3000" style={{transformOrigin: '0px 0px'}}>
+          <div className="contact-info">
+            <h3 className="title">Let's get connected</h3>
+            <p className="text">
+              Green cards, officially known as Permanent Resident Cards, allow non-U.S. citizens to live and work permanently in the United States. Holding a green card is a prerequisite for applying for U.S. citizenship .
+            </p>
+            <div className="info">
+              <div className="information">
+                <img src={Location} className="icon" alt="" />
+                <p style={{fontSize:'1rem'}}>United States of America</p>
+              </div>
+            </div>
+            <p className='FAQ' style={{fontSize:'.7rem', paddingTop:'.8rem', marginBottom:'-.5rem'}}>Have any question or suggestion! Please let us know.</p>
+          </div>
+
+          <div className="contact-form">
+            <span className="circle one"></span>
+            <span className="circle two"></span>
+            <form onSubmit={sendEmail}>
+              <h3 className="title">Contact Us</h3>
+              <div className="input-container-c">
+                <input type="text" name="name" className="input" placeholder='Name' pattern="[a-zA-Z]{3,50}" required
+                  onChange={(e)=>setName(e.target.value)} />
+              </div>
+              <div className="input-container-c">
+                <input type="email" name="email" className="input" placeholder='Email' pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required
+                  onChange={(e)=>setEmail(e.target.value)} />
+              </div>
+              <div className="input-container-c textarea">
+                <textarea name="message" className="input" placeholder='Message' pattern="[a-zA-Z]{500}" required onChange={(e)=>setMessage(e.target.value)}></textarea>
+              </div>
+              <input type="submit" value="Send" className="button s-button" />
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className="blur-c"></div>
+    </div>
+  );
+}
+
+export default Contact;
